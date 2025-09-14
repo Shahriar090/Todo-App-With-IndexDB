091241108988adf05e5c69242936b136447b3cc1
Shahriar Hossain <shahriarh655@gmail.com>
2025-09-14 13:39:48 +0600

    refactor(web): refactored TodoProvider's add todo logic. now it is sync with indexdb instead of local state

--- src/providers/TodoProvider.tsx
+++ src/providers/TodoProvider.tsx
@@ -1,24 +1,39 @@
 import { TodoContext } from '@/context';
 import { db, type TODO } from '@/db/db';
+import { useAuth } from '@/hooks/useAuth';
+import { useLiveQuery } from 'dexie-react-hooks';
 import type React from 'react';
-import { useEffect, useState } from 'react';
 
 const TodoProvider = ({ children }: { children: React.ReactNode }) => {
-	const [todos, setTodos] = useState<TODO[]>([]);
+	// const [todos, setTodos] = useState<TODO[]>([]);
 
 	// get todos for initial load
-	useEffect(() => {
-		const fetchTodos = async () => {
-			const allTodos = await db.todos.toArray();
-			setTodos(allTodos);
-		};
-		fetchTodos();
-	}, []);
+	// useEffect(() => {
+	// 	const fetchTodos = async () => {
+	// 		const allTodos = await db.todos.toArray();
+	// 		setTodos(allTodos);
+	// 	};
+	// 	fetchTodos();
+	// }, []);
+
+	// updated logic after adding authentication
+	const { user } = useAuth();
+
+	const todos = useLiveQuery(async () => {
+		if (!user?.id) return [];
+
+		// only fetch todos for current authenticated user
+		return await db.todos.where('userId').equals(user.id).toArray();
+	}, [user?.id]);
 
 	// add new todo
-	const addTodo = async (payload: Omit<TODO, 'id'>) => {
-		await db.todos.add({ ...payload });
-		setTodos(await db.todos.toArray());
+	const addTodo = async (payload: Omit<TODO, 'id' | 'userId'>) => {
+		if (!user?.id) {
+			throw new Error('User must be authenticated to add todos');
+		}
+		await db.todos.add({ ...payload, userId: user.id });
+
+		// setTodos(await db.todos.toArray());
 	};
 
 	// update a todo
